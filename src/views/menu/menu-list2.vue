9c3f4281a80ef801c3dc9ed42e04189ee6a2d540
hyx0217 <hyx1014959288@163.com>
2020-08-12 17:46:10 +0800

    update

--- src/views/menu/menu-list2.vue
+++ src/views/menu/menu-list2.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>菜单2</div>
-</template>;
+  <div>
+    <h2>Script Setup + CSS Variables Injection</h2>
+    <p>
+      <span class="style-vars">This should be red</span>
+      <button class="script-setup-change"
+              @click="change">click to change</button>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+export default {
+  props: {
+    msg: String
+  }
+};
+export const color = ref('red');
+export function change() {
+  color.value = color.value === 'red' ? 'green' : 'red';
+}
+</script>
+
+<style scoped vars="{ color }">
+span {
+  color: var(--color);
+}
+</style>
